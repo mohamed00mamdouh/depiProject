3db6300f5f7da01d347c86ff6f7fd9b0fbc6ac07
mohamed00mamdouh <00mansionmohamed00@gmail.com>
2024-09-11 18:49:10 +0300

    showing users from db in Dashboard

--- crud-node/src/routes/userRoute.js
+++ crud-node/src/routes/userRoute.js
@@ -6,4 +6,6 @@ const router = express.Router()
 
 router.post("/",userController.createUser)
 
+router.get("/", userController.getAllUsers)
+
 module.exports= router
--- crud-react/src/components/dashboard/dashboard.js
+++ crud-react/src/components/dashboard/dashboard.js
@@ -1,7 +1,50 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { Col, Row, Table, Container } from 'react-bootstrap';
+
 const Dashboard= () => {
+    const [users, setUsers]= useState([])
+    useEffect(() => {
+        const fetchUsers= async () =>{
+            try {
+                const response= await fetch("http://localhost:5000/api/user")
+                const data = await response.json()
+                setUsers(data)
+            } catch (error) {
+                console.error(`error while fetching users: ${error.message}`);
+                
+            }
+        }
+        fetchUsers()
+    }, [])
     return (
         <>
-            <h1>Dashboard Component</h1>
+            <Container className='mt-5'>
+                <Row>
+                    <Col>
+                        <h1 className='text-center'>Dashboard Component</h1>
+                        <Table striped border hover responsive>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    {/* <th>Action</th> */}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {users.map((user) =>(
+                                    <tr key={user._id}>
+                                        <td>{user.name}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.phone}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
